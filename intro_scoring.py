c677c0ba5d02955ad1e98edab78157841bf90a13
Sergey Titov <sstmail@mail.ru>
2018-11-08 01:34:59 +0300

    feat: print out unique entries

--- intro_scoring.py
+++ intro_scoring.py
@@ -8,3 +8,12 @@ plt.style.use('ggplot')
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data'
 data = pd.read_csv(url, header=None, na_values='?')
 data.shape
+data.columns = ['A' + str(i) for i in range(1, 16)] + ['class']
+
+#%%
+categorical_columns = [c for c in data.columns if data[c].dtype.name == 'object']
+numerical_columns   = [c for c in data.columns if data[c].dtype.name != 'object']
+print(categorical_columns)
+print(numerical_columns)
+for c in categorical_columns:
+    print(data[c].unique())
